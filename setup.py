29e28fcb10c5dd74f76abcab76a491b1e87a58f1
AnkitKumarGautam2003 <76133610+AnkitKumarGautam2003@users.noreply.github.com>
2024-09-29 23:20:01 +0530

    My first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="scrapper",
+    version="0.0.1",
+    author="Ankit",
+    author_email="ak8712117@gmail.com",
+    packages=find_packages(),
+    insall_requires=[]
+) 
+
